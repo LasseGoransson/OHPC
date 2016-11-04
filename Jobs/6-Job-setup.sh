35dc7a282b7a6f024923aaf65742d334dcb2f614
LasseG <lasse973@gmail.com>
2016-11-04 23:50:02 +0100

    initial

--- Jobs/6-Job-setup.sh
+++ Jobs/6-Job-setup.sh
@@ -24,24 +24,24 @@ source /etc/profile.d/OpenHPC_enviroment_variables.sh
 
 #exit 0
 echo ">>>>>>>>>> Install OpenMPI LAMMPS"
-yum --installroot=$CHROOT  remove lmod-ohpc 
-yum remove lmod-ohpc 
-yum --installroot=$CHROOT install /home/user/Jobs/*.rpm
-yum install /home/user/Jobs/*.rpm
+yum -y --installroot=$CHROOT  remove lmod-ohpc 
+yum -y remove lmod-ohpc 
+yum -y --installroot=$CHROOT install /home/user/Jobs/*.rpm
+yum -y install /home/user/Jobs/*.rpm
 
-yum --installroot=$CHROOT install libgfortran libgomp python-virtualenv
-yum install libgfortran libgomp python-virtualenv
+yum -y --installroot=$CHROOT install libgfortran libgomp python-virtualenv
+yum -y install libgfortran libgomp python-virtualenv
 
-rpm --root=$CHROOT --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro 
-rpm --root=$CHROOT -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-1.el7.nux.noarch.rpm
+rpm -y --root=$CHROOT --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro 
+rpm -y --root=$CHROOT -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-1.el7.nux.noarch.rpm
 
 rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro 
 rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-1.el7.nux.noarch.rpm
 
-yum --installroot=$CHROOT makecache
+yum -y --installroot=$CHROOT makecache
 yum  makecache
 
-yum --installroot=$CHROOT install ffmpeg
-yum  install ffmpeg
+yum -y --installroot=$CHROOT install ffmpeg
+yum -y install ffmpeg
 
 
