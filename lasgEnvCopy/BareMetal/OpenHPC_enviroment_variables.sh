97dbe40864cd4e459e7c3b92da57c8ae011d0821
Lasse G <lasse973@mail.com>
2017-04-20 00:03:34 +0200

    Added comments to enviroments

--- lasgEnvCopy/BareMetal/OpenHPC_enviroment_variables.sh
+++ lasgEnvCopy/BareMetal/OpenHPC_enviroment_variables.sh
@@ -4,14 +4,21 @@ export sms_name=sms-server
 export sms_ip=192.168.99.254
 export internal_netmask=255.255.255.0
 export ntp_server=sms-server
+
+# BMC information
 export bmc_ipaddr=(192.168.100.20 192.168.100.21)
 export bmc_username=USERID
 export bmc_password=PASSW0RD
+
+# Node information
 export num_computes=2
 export base_computes=node
 export compute_regex=${base_computes}*
 export c_ip=("192.168.99.1" "192.168.99.2")
 export c_name=("node1" "node2")
+export c_mac=(e4:1f:13:22:85:14 E4:1F:13:22:7C:B1)
+
+
+# Interface configuration
 export sms_eth_internal=enp11s0f1
 export eth_provision=enp11s0f1
-export c_mac=(e4:1f:13:22:85:14 E4:1F:13:22:7C:B1)
